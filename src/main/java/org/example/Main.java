c866833830533ffbade86dd4fc10aae1da2a5adc
Daniil Dumshenko <dumshenko2003@gmail.com>
2024-10-14 09:31:22 +0200

    Forgot to add pull request

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,28 +4,6 @@ public class Main {
 
     public static void main(String[] args){
 
-        daysOfWeek today = daysOfWeek.FRI;
 
-        String currentDay = switch (today) {
-            case MO -> {
-                yield "Today is Monday";
-            }
-            case TUE -> {
-                yield "Today is Tuesday";
-            }
-            case WED -> {
-                yield "Today is Wednesday";
-            }
-            case THU -> {
-                yield "Today is Thursday";
-            }
-            case FRI -> {
-                yield "Today is Friday";
-            }
-            case SAT, SUN -> {
-                yield "Today is Weekend";
-            }
-            };
-        System.out.println(currentDay);
         }
     }
